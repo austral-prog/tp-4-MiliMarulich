cfb2c3dc5216a15bbbd88250204720dc8f74f767
MiliMarulich <mmarulich@mail.austral.edu.ar>
2025-04-11 16:42:40 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,7 +1,7 @@
 import math
 
 def line():
-    a = float(input("Ingrese el coeficiente A: ")) #Python siempre los toma como texto (string). float("3") te devuelve 3.0, que sigue siendo válido. También podés usar int() si solo querés números enteros, pero float() es más general porque acepta decimales e enteros.
+    a = float(input("Ingrese el coeficiente A: ")) 
     b = float(input("Ingrese el coeficiente B: "))
     x1 = float(input("Ingrese el coeficiente X1: "))
     x2 = float(input("Ingrese el coeficiente X2: "))
@@ -9,12 +9,17 @@ def line():
     print(f"El coeficiente B de su ecuación de la recta es: {b}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
+
+    eq = f"Y = {a}X + {b}"
     print(f"\nPara la siguiente ecuación: ")
-    print(f"\tY = {a}x + {b}")
-    print(f"\nDados los siguientes puntos: ")
+    print(f"\t{eq}\n")
+
     y1 = (a*x1 + b)
     y2 = (a*x2 + b)
+
+    print("Dados los siguientes puntos: ")
     print(f"\tP1 ({x1}, {y1})")
     print(f"\tP2 ({x2}, {y2})")
-    distancia = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     print(f"\nLa distancia entre ellos es: {distancia}")
